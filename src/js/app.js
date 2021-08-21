a994c42c5c0d3d327208cd97bb3f90435a93cf3f
moehzi <faizi1234098@gmail.com>
2021-08-21 16:05:56 +0800

    about and gallery responsive

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,6 @@
 const menu = document.querySelector("#mobile-menu");
 const menuLinks = document.querySelector(".menu__container");
-const scrollDown = document.querySelector(".scroll-down-button");
+
 menu.addEventListener("click", function () {
   menu.classList.toggle("is-active");
   menuLinks.classList.toggle("active");
