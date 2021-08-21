e135b2d895eeee033f2fc9090b42e0ed929d19a2
moehzi <faizi1234098@gmail.com>
2021-08-21 10:52:35 +0800

    add smooth scroll

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,7 @@
-const menu = document.querySelector('#mobile-menu');
-const menuLinks = document.querySelector('.menu__container');
-menu.addEventListener('click',function(){
-	menu.classList.toggle('is-active');
-	menuLinks.classList.toggle('active')
+const menu = document.querySelector("#mobile-menu");
+const menuLinks = document.querySelector(".menu__container");
+const scrollDown = document.querySelector(".scroll-down-button");
+menu.addEventListener("click", function () {
+  menu.classList.toggle("is-active");
+  menuLinks.classList.toggle("active");
 });
